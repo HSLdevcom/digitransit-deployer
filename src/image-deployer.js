ab31d72acfc9e62bb552214dbcf86d2feaa1087e
Sami Siren <siren@foofactory.fi>
2017-02-23 20:44:54 +0200

    do not update when package or deps unstable

--- src/image-deployer.js
+++ src/image-deployer.js
@@ -1,4 +1,5 @@
 const debug = require('debug')('digitransit-image-deployer');
+const graph = require('./graph.js');
 
 const logError=(e) => {
   debug("Error occurred %s", JSON.stringify(e));
@@ -10,16 +11,22 @@ const logError=(e) => {
 module.exports = {
   name:'image-deployer',
   command:(services, context) => {
+    let serviceGraph = graph.build(services);
+
     services.filter((service) => service.labels['update'] === 'auto' && service.container.docker.forcePullImage === true)
     .forEach(service => {
       context.dockerRepo.getManifest(service.container.docker.image).then(response => {
-        const imageDate = Date.parse(JSON.parse(response.manifest.history[0].v1Compatibility).created);
+        const imageDate = Date.parse(response.manifest.history[0].v1Compatibility.created);
         const serviceDate = Date.parse(service.version);
         const needsUpdate = imageDate > serviceDate;
-        debug("%s needs update: %s", service.id, needsUpdate);
+        debug("%s needs update: %s, %s > %s", service.id, needsUpdate, imageDate, serviceDate);
         if(needsUpdate) {
-          debug("restarting service %s", service.id);
-          context. marathon.restartService(service.id).then((e) => debug("restart called %s", e));
+          if(graph.isSubGraphStable(serviceGraph, service.id)) {
+            debug("restarting service %s", service.id);
+            context. marathon.restartService(service.id).then((e) => debug("restart called: %s", e));
+          } else {
+            debug("delaying restart for %s (subgraph not stable)", service.id);
+          }
         }
       }).catch(logError);
     });
