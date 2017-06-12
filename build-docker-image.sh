54e11769a627d26590de8e02a0f2f9e9be994195
Sami Siren <siren@foofactory.fi>
2017-06-12 10:11:51 +0300

    all logic for different stages (pr/master/tag)

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
-ORG=hsldevcom
+ORG=${ORG:-hsldevcom}
 DOCKER_IMAGE=digitransit-deployer
 
+DOCKER_TAG="ci-${TRAVIS_COMMIT}"
 # Set these environment variables
-DOCKER_TAG=${VARIABLE:-latest}
 #DOCKER_EMAIL=
 #DOCKER_USER=
 #DOCKER_AUTH=
 
-# Build image
-docker build --tag="$ORG/$DOCKER_IMAGE:$DOCKER_TAG" .
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_AUTH
-docker push $ORG/$DOCKER_IMAGE:$DOCKER_TAG
+function tagandpush
+  docker tag $ORG/$DOCKER_IMAGE:$DOCKER_TAG $ORG/$DOCKER_IMAGE:$1
+  docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_AUTH
+  docker push $ORG/$DOCKER_IMAGE:$DOCKER_TAG $ORG/$DOCKER_IMAGE:$1
+fi
+
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+
+  if [ -n $TRAVIS_TAG ];then
+    #release do not rebuild, just tag
+    docker pull $ORG/$DOCKER_IMAGE:$DOCKER_TAG
+    tagandpush "prod"
+  else
+    #master branch, build and tag as latest
+    docker build --tag="$ORG/$DOCKER_IMAGE:$DOCKER_TAG" .
+    tagandpush "latest"
+  fi
+else
+  #pr just build
+  docker build --tag="$ORG/$DOCKER_IMAGE:$DOCKER_TAG" .
+fi
