6b768d4da6e842e37eb0eb929bfdec75aa5d4ff2
Sami Siren <siren@foofactory.fi>
2017-08-03 23:07:39 +0300

    fix script

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -16,7 +16,7 @@ function tagandpush {
 
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
 
-  if [ -n $TRAVIS_TAG ];then
+  if [ "$TRAVIS_TAG" ];then
     echo "processing release $TRAVIS_TAG"
     #release do not rebuild, just tag
     docker pull $ORG/$DOCKER_IMAGE:$DOCKER_TAG
