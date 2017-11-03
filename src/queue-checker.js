341b21cd265cae5b9069a841e4d35599716146e7
Joel Lappalainen <joel.lappalainen@cgi.com>
2017-11-03 16:24:33 +0200

    document queue-checker

--- src/queue-checker.js
+++ src/queue-checker.js
@@ -3,6 +3,10 @@ const {postSlackMessage} = require('./util');
 
 const COOL_OFF_PERIOD = 45*60*1000; //45 minutes
 
+/*
+ * Reports to slack if a service deployment is in waiting state after a period
+ * defined in COOL_OFF_PERIOD.
+ */
 module.exports = {
   name:'queue-checker',
   command: (deployments) => {
