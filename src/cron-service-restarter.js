4783de61a3decfe518823773d32da874a18493a2
Joel Lappalainen <joel.lappalainen@cgi.com>
2017-12-15 18:25:22 +0200

    updated comments and prevent multiple restarts of the same service during one check

--- src/cron-service-restarter.js
+++ src/cron-service-restarter.js
@@ -5,6 +5,8 @@ const time = require('time')(Date);
 /*
  * Automatically restarts services if they have a restart-at label defined.
  * Restart time is defined in the restart-at label with "hh:mm" format.
+ * It is possible to define multiple restart points by separating them with commas,
+ * for example "13:00, 18:50".
  * Service will not be restarted if it has been less than minutes defined
  * in label restart-limi-interval or 18 hours, if the label hasn't been defined,
  * since the last restart. The last chance for the restart to occur is 60 minutes
@@ -24,6 +26,7 @@ module.exports = {
   command: (services, context) => {
     let serviceGraph = graph.build(services);
     const NOW = new Date().getTime();
+    let hasBeenRestarted = false;
 
     services.filter((service) => service.labels['restart-at'])
     .forEach(service => {
@@ -31,27 +34,30 @@ module.exports = {
       const restartIntervalMins = parseInt(service.labels['restart-limit-interval']) || 60 * 18;
 
       service.labels['restart-at'].split(',').forEach(restartTime => {
-        const trimmedTime = restartTime.replace(/\s/g, '');
-        const timeArray = trimmedTime.split(':');
-        const nextHour = parseInt(timeArray[0]) + 1;
-  
-        const cronDate = getDateObject(timeArray);
-        // One hour later
-        const cronDateUpperLimit = getDateObject([nextHour, timeArray[1]]);
+        if (!hasBeenRestarted) {
+          const trimmedTime = restartTime.replace(/\s/g, '');
+          const timeArray = trimmedTime.split(':');
+          const nextHour = parseInt(timeArray[0]) + 1;
 
-        if (NOW - serviceDate >= restartIntervalMins * 60 * 1000 &&
-          NOW >= cronDate.getTime() &&
-          NOW <= cronDateUpperLimit.getTime()) {
-          if(graph.isSubGraphStable(serviceGraph, service.id)) {
-            debug("Restarting service %s", service.id);
-            context.marathon.restartService(
-              service.id).then((r) => debug("Restart called: %s", JSON.stringify(r))
-            );
+          const cronDate = getDateObject(timeArray);
+          // One hour later
+          const cronDateUpperLimit = getDateObject([nextHour, timeArray[1]]);
+
+          if (NOW - serviceDate >= restartIntervalMins * 60 * 1000 &&
+            NOW >= cronDate.getTime() &&
+            NOW <= cronDateUpperLimit.getTime()) {
+            if(graph.isSubGraphStable(serviceGraph, service.id)) {
+              debug("Restarting service %s", service.id);
+              context.marathon.restartService(
+                service.id).then((r) => debug("Restart called: %s", JSON.stringify(r))
+              );
+              hasBeenRestarted = true;
+            } else {
+              debug("Delaying restart for %s (subgraph not stable)", service.id);
+            }
           } else {
-            debug("Delaying restart for %s (subgraph not stable)", service.id);
+            debug("No need to update %s", service.id);
           }
-        } else {
-          debug("No need to update %s", service.id);
         }
       });
     });
