10d596d4102e135294e88760676da4b187f28b05
Sami Siren <siren@foofactory.fi>
2017-12-19 15:31:42 +0200

    not used

--- src/test.js
+++
@@ -1,7 +0,0 @@
-const debug = require('debug')('test');
-const dockerRepo =  require('./dockerRepo');
-
-dockerRepo.getImageDate("hsldevcom/opentripplanner:prod").then(response => {
-  //debug(response.res);
-  debug(response);
-});
