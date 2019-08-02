dce7b3fcb549c1feb35968a6ea06167871074086
Joel Lappalainen <joel.lappalainen@cgi.com>
2019-08-02 21:42:14 +0300

    Define default namespace

--- src/kubernetes.js
+++ src/kubernetes.js
@@ -5,24 +5,35 @@ const backend = new Request(Request.config.getInCluster())
 const client = new Client({ backend })
 
 const getDeployments = () => {
-  client.loadSpec()
-    .then(() => {
-      client.apis.apps.v1.deployments().get()
-        .then((deployments) => {
-          return deployments.body.items
-        })
-    })
+  return new Promise((resolve, reject) => {
+    client.loadSpec()
+      .then(() => {
+        client.apis.apps.v1.namespaces('default').deployments().get()
+          .then((deployments) => {
+            client.apis.apps.v1.namespaces('default').pods().get()
+              .then((pods) => {
+                pods.forEach((pod) => {
+                  console.log(pod)
+                  resolve([])
+                })
+              })
+          })
+      })
+      .catch((err) => {
+        reject(err)
+      })
+  })
 }
 
 // Restart by modifying deployment label because kubernetes does not have deployment restart functionality in 1.13 API version
 const restartDeployment = (appName) => {
   client.loadSpec()
     .then(() => {
-      client.apis.apps.v1.deployments()
+      client.apis.apps.v1.namespaces('default').deployments()
         .patch({ qs: { labelSelector: `app=${appName}` }, body: { spec: { template: { metadata: { labels: { lastRestartDate: Date.now() } } } } } })
         .then((deployments) => {
           deployments.body.items.forEach((item) => {
-            console.log(item.metadata)
+            console.log(item)
           })
         })
     })
