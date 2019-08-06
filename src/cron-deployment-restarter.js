82485b6656fe0044db6f6e63e73e37c9873795c3
Joel Lappalainen <joel.lappalainen@cgi.com>
2019-08-06 17:40:33 +0300

    Rewrite cron-deployment-restarter to work with kubernetes

--- src/cron-deployment-restarter.js
+++ src/cron-deployment-restarter.js
@@ -2,14 +2,15 @@ const debug = require('debug')('cron-deployment-restarter.js')
 const graph = require('./graph.js')
 
 /*
- * Automatically restarts services if they have a restart-at label defined.
- * Restart time is defined in the restart-at label with "hh:mm" format.
+ * Automatically restarts deployments if they have a restartAt label defined.
+ * Restart time is defined in the restartAt label with "hh:mm" format.
  * It is possible to define multiple restart points by separating them with commas,
  * for example "13:00, 18:50".
- * Service will not be restarted if it has been less than minutes defined
- * in label restart-limi-interval or 18 hours, if the label hasn't been defined,
- * since the last restart. The last chance for the restart to occur is 60 minutes
- * past the time defined in restart-at label.
+ * Deployment will not be restarted if it has not been at least 18 hours (by default)
+ * since the last restart of the deployment. Alternatively, you can define in
+ * restartLimitInterval label how many minutes should be passed since the last restart before
+ * another restart can occur.
+ * The last chance for the restart to occur is 60 minutes past the time defined in restart-at label.
  */
 
 const getDateObject = ([hour, minute]) => {
@@ -21,17 +22,18 @@ const getDateObject = ([hour, minute]) => {
 
 module.exports = {
   name: 'cron-deployment-restarter',
-  command: (services, context) => {
-    let serviceGraph = graph.build(services)
+  command: (deployments, context) => {
+    let deploymentGraph = graph.build(deployments)
     const NOW = new Date().getTime()
     let hasBeenRestarted = false
 
-    services.filter((service) => service.labels['restart-at'])
-      .forEach(service => {
-        const serviceDate = Date.parse(service.version)
-        const restartIntervalMins = parseInt(service.labels['restart-limit-interval']) || 60 * 18
+    deployments.filter((deployment) => deployment.spec.template.metadata.labels['restartAt'])
+      .forEach(deployment => {
+        const deploymentDate = Date.parse(deployment.version)
+        const restartIntervalMins =
+          parseInt(deployment.spec.template.metadata.labels['restartLimitInterval']) || 60 * 18
 
-        service.labels['restart-at'].split(',').forEach(restartTime => {
+        deployment.spec.template.metadata.labels['restartAt'].split(',').forEach(restartTime => {
           if (!hasBeenRestarted) {
             const trimmedTime = restartTime.replace(/\s/g, '')
             const timeArray = trimmedTime.split(':')
@@ -41,20 +43,22 @@ module.exports = {
             // One hour later
             const cronDateUpperLimit = getDateObject([nextHour, timeArray[1]])
 
-            if (NOW - serviceDate >= restartIntervalMins * 60 * 1000 &&
+            if (NOW - deploymentDate >= restartIntervalMins * 60 * 1000 &&
             NOW >= cronDate.getTime() &&
             NOW <= cronDateUpperLimit.getTime()) {
-              if (graph.isSubGraphStable(serviceGraph, service.id)) {
-                debug('Restarting service %s', service.id)
-                context.marathon.restartService(
-                  service.id).then((r) => debug('Restart called: %s', JSON.stringify(r))
-                )
-                hasBeenRestarted = true
+              if (graph.isSubGraphStable(deploymentGraph, deployment.metadata.labels.app)) {
+                debug('Restarting deployment %s', deployment.metadata.labels.app)
+                context.kubernetes.restartDeployment(deployment.metadata.labels.app)
+                  .then((r) => {
+                    debug('Restart called: %s', JSON.stringify(r))
+                    hasBeenRestarted = true
+                  })
+                  .catch((err) => debug(err))
               } else {
-                debug('Delaying restart for %s (subgraph not stable)', service.id)
+                debug('Delaying restart for %s (subgraph not stable)', deployment.metadata.labels.app)
               }
             } else {
-              debug('No need to update %s', service.id)
+              debug('No need to update %s', deployment.metadata.labels.app)
             }
           }
         })
