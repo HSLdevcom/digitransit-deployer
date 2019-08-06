938d5efe77444513695c1ed89252c7b69ac10167
Joel Lappalainen <joel.lappalainen@cgi.com>
2019-08-06 17:39:10 +0300

    Fix issues with kubernetes API calls and implement deployment getter and restarter

--- src/kubernetes.js
+++ src/kubernetes.js
@@ -1,6 +1,8 @@
 const { Client, KubeConfig } = require('kubernetes-client')
 const Request = require('kubernetes-client/backends/request')
 
+const debug = require('debug')('kubernetes')
+
 const kubeconfig = new KubeConfig()
 kubeconfig.loadFromCluster()
 const backend = new Request({ kubeconfig })
@@ -12,12 +14,35 @@ const getDeployments = () => {
       .then(() => {
         client.apis.apps.v1.namespaces('default').deployments().get()
           .then((deployments) => {
-            client.apis.apps.v1.namespaces('default').pods().get()
+            client.api.v1.namespaces('default').pods().get()
               .then((pods) => {
-                pods.forEach((pod) => {
-                  console.log(pod)
-                  resolve([])
+                const deploymentVersions = {}
+                pods.body.items.forEach((pod) => {
+                  if (pod.metadata !== undefined &&
+                    pod.metadata.labels !== undefined &&
+                    pod.metadata.labels.app !== undefined &&
+                    pod.status !== undefined &&
+                    pod.status.startTime !== undefined &&
+                    (deploymentVersions[pod.metadata.labels.app] === undefined ||
+                      pod.status.startTime < deploymentVersions[pod.metadata.labels.app])) {
+                    deploymentVersions[pod.metadata.labels.app] = pod.status.startTime
+                  }
                 })
+
+                const patchedDeployments = []
+                deployments.body.items.forEach((deployment) => {
+                  if (deployment.metadata !== undefined &&
+                    deployment.metadata.labels !== undefined &&
+                    deployment.metadata.labels.app !== undefined &&
+                    deploymentVersions[deployment.metadata.labels.app] !== undefined) {
+                    patchedDeployments.push(
+                      { ...deployment,
+                        version: deploymentVersions[deployment.metadata.labels.app] })
+                  } else {
+                    debug('Could not find pods for deployment %s', JSON.stringify(deployment))
+                  }
+                })
+                resolve(patchedDeployments)
               })
           })
       })
@@ -28,17 +53,24 @@ const getDeployments = () => {
 }
 
 // Restart by modifying deployment label because kubernetes does not have deployment restart functionality in 1.13 API version
-const restartDeployment = (appName) => {
-  client.loadSpec()
-    .then(() => {
-      client.apis.apps.v1.namespaces('default').deployments()
-        .patch({ qs: { labelSelector: `app=${appName}` }, body: { spec: { template: { metadata: { labels: { lastRestartDate: Date.now() } } } } } })
-        .then((deployments) => {
-          deployments.body.items.forEach((item) => {
-            console.log(item)
+const restartDeployment = (appId) => {
+  return new Promise((resolve, reject) => {
+    client.loadSpec()
+      .then(() => {
+        client.apis.apps.v1.namespaces('default').deployments()
+          .patch({
+            qs: { labelSelector: `app=${appId}` },
+            body: { spec: { template: { metadata: { labels: { lastRestartDate: Date.now() } } } } }
           })
-        })
-    })
+          .then(() => {
+            resolve(appId)
+          })
+      })
+      .catch((err) => {
+        debug('Failed to restart %s', appId)
+        reject(err)
+      })
+  })
 }
 
 module.exports = {
