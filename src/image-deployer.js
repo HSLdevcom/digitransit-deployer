ce5dbb3afa87db00180cd1fb9c4251167a598041
Joel Lappalainen <joel.lappalainen@cgi.com>
2019-08-06 17:39:43 +0300

    Rewrite image-deployer to work with kubernetes

--- src/image-deployer.js
+++ src/image-deployer.js
@@ -8,46 +8,50 @@ const logError = (e) => {
 const COOL_OFF_PERIOD = 60 * 60 * 1000 // 1 hour
 
 /*
- * Automatically deploys new container versions for services that are tagged with update=auto and have forcepullimage configured as true.
- * Also restarts service if one of the dependency images/tags defined in label restart-after-image-updates are updated.
+ * Automatically deploys new container versions for deployments that are tagged with update=auto and have imagePullPolicy configured as Always.
+ * Also restarts deployment if one of the dependency images/tags defined in label restartAfterImageUpdates are updated.
  */
 module.exports = {
   name: 'image-deployer',
-  command: function (services, context) {
-    let serviceGraph = graph.build(services)
+  command: function (deployments, context) {
+    let deploymentGraph = graph.build(deployments)
 
     const NOW = new Date().getTime()
 
-    services.filter((service) => service.labels.hasOwnProperty('restart-after-image-updates') ||
-      (service.labels['update'] === 'auto' && service.container.docker.forcePullImage === true))
-      .forEach(service => {
+    deployments.filter((deployment) => deployment.spec.template.metadata.labels['restartAfterImageUpdates'] ||
+      (deployment.spec.template.metadata.labels['update'] === 'auto' &&
+      deployment.spec.template.spec.containers[0].imagePullPolicy === 'Always'))
+      .forEach(deployment => {
         let dependencies = []
-        if (service.labels.hasOwnProperty('restart-after-image-updates')) {
-          dependencies = dependencies.concat(service.labels['restart-after-image-updates'].replace(/\s/g, '').split(','))
+        if (deployment.spec.template.metadata.labels['restartAfterImageUpdates']) {
+          dependencies =
+            dependencies.concat(
+              deployment.spec.template.metadata.labels['restartAfterImageUpdates'].replace(/\s/g, '').split(','))
         }
-        if (service.labels['update'] === 'auto' && service.container.docker.forcePullImage === true) {
-          dependencies.push(service.container.docker.image)
+        if (deployment.spec.template.metadata.labels['update'] === 'auto' &&
+          deployment.spec.template.spec.containers[0].imagePullPolicy === 'Always') {
+          dependencies.push(deployment.spec.template.spec.containers[0].image)
         }
         const promises = []
         for (let i = 0; i < dependencies.length; i++) {
           const dependency = dependencies[i]
           promises.push(new Promise((resolve) => {
             context.dockerRepo.getImageDate(dependency).then(repoImageDate => {
-              const serviceDate = Date.parse(service.version)
-              if (repoImageDate > serviceDate) {
-                if (NOW > serviceDate + COOL_OFF_PERIOD) {
-                  if (graph.isSubGraphStable(serviceGraph, service.id)) {
+              const deploymentDate = Date.parse(deployment.version)
+              if (repoImageDate > deploymentDate) {
+                if (NOW > deploymentDate + COOL_OFF_PERIOD) {
+                  if (graph.isSubGraphStable(deploymentGraph, deployment.metadata.labels.app)) {
                     resolve('restart')
                   } else {
-                    debug('Delaying restart for %s (subgraph not stable)', service.id)
+                    debug('Delaying restart for %s (subgraph not stable)', deployment.metadata.labels.app)
                     resolve(null)
                   }
                 } else {
-                  debug('Delaying restart for %s (cool off period)', service.id)
+                  debug('Delaying restart for %s (cool off period)', deployment.metadata.labels.app)
                   resolve(null)
                 }
               } else {
-                debug('No need to update %s', service.id)
+                debug('No need to update %s', deployment.metadata.labels.app)
                 resolve(null)
               }
             }).catch((err) => {
@@ -58,8 +62,8 @@ module.exports = {
         }
         Promise.all(promises).then((values) => {
           if (values.indexOf('restart') >= 0) {
-            debug('Restarting service %s', service.id)
-            context.marathon.restartService(service.id)
+            debug('Restarting deployment %s', deployment.metadata.labels.app)
+            context.kubernetes.restartDeployment(deployment.metadata.labels.app)
               .then((r) => debug('Restart called: %s', JSON.stringify(r)))
               .catch((err) => debug(err))
           }
