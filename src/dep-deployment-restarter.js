586b94deb6c112850fe0c23409f3eab0e1d12839
Joel Lappalainen <joel.lappalainen@cgi.com>
2019-08-06 17:40:46 +0300

    Rewrite dep-deployment-restarter to work with kubernetes

--- src/dep-deployment-restarter.js
+++ src/dep-deployment-restarter.js
@@ -3,45 +3,45 @@ const graph = require('./graph.js')
 const { postSlackMessage } = require('./util')
 
 /*
- * Automatically restarts dependand services in controlled manner. This is
+ * Automatically restarts dependand deployments in controlled manner. This is
  * useful because we have data as container and we want to restart containers
  * that depend on that data.
  * Configured with labels as follows:
- *  restart-after-services=/service-name
- *  restart-delay=5
+ *  restartAfterDeployments=/deployment-name
+ *  restartDelay=5
  *
  *  or multiple:
- *  restart-after-services=/service-name,/another-service
- *  restart-delay=5
+ *  restartAfterDeployments=/deployment-name,/another-deployment
+ *  restartDelay=5
  *
- * /service-name is the dependent mesos service name (including the
+ * /deployment-name is the dependent mesos deployment name (including the
  * leading '/').
  * restart-delay is in minutes, specifying this to 1 means that restart is not
  * triggered before 1 minutes have elapsed from restarting of the dependent
- * service. This also means that if service has restarted during delay period
+ * deployment. This also means that if deployment has restarted during delay period
  * it will be restarted after dependency restart time + delay has passed.
  *
- * Additionally the subgraph of dependencies including the service at hand must
+ * Additionally the subgraph of dependencies including the deployment at hand must
  * all be in stable condition before restart is attempted.
  *
- * Additionally the dependent mesos service (or any of it's parents) must not
+ * Additionally the dependent mesos deployment (or any of it's parents) must not
  * have any pending restarts waiting.
  *
  */
 module.exports = {
   name: 'dep-deployment-restarter',
-  command: (services, context) => {
+  command: (deployments, context) => {
     const NOW = new Date().getTime()
 
-    let serviceGraph = graph.build(services)
-    if (serviceGraph.hasCycle()) {
-      debug('Bummer! Graph has cycle, %s', serviceGraph.toJSON())
-      postSlackMessage('Services are configured to restart each other in a cycle.')
+    let deploymentGraph = graph.build(deployments)
+    if (deploymentGraph.hasCycle()) {
+      debug('Bummer! Graph has cycle, %s', deploymentGraph.toJSON())
+      postSlackMessage('Deployments are configured to restart each other in a cycle.')
     } else {
-      graph.servicesNeedingRestart(serviceGraph).filter(({ from, value }) => {
-        debug('service %s needs restart', from)
+      graph.deploymentsNeedingRestart(deploymentGraph).filter(({ from, value }) => {
+        debug('deployment %s needs restart', from)
         // check that enough time has passed after all depedency restarts
-        for (let [, vertexValue] of serviceGraph.verticesFrom(from)) {
+        for (let [, vertexValue] of deploymentGraph.verticesFrom(from)) {
           debug('checking %s %s', NOW, Date.parse(vertexValue.version))
           if (NOW <= Date.parse(vertexValue.version) + value.delay) {
             return false
@@ -49,14 +49,16 @@ module.exports = {
         }
         return true
       }).filter(({ from }) => {
-        if (!graph.isSubGraphStable(serviceGraph, from)) {
+        if (!graph.isSubGraphStable(deploymentGraph, from)) {
           debug('Sub Graph for %s is not stable, delaying restart', from)
           return false
         }
         return true
       }).forEach(({ from }) => {
-        debug('Restarting service %s', from)
-        context.marathon.restartService(from).then((e) => debug('Restarted: %s', JSON.stringify(e)))
+        debug('Restarting deployment %s', from)
+        context.kubernetes.restartDeployment(from)
+          .then((e) => debug('Restarted: %s', JSON.stringify(e)))
+          .catch((err) => debug(err))
       })
     }
   }
