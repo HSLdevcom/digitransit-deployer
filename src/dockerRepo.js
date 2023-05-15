ffb1f059075fa6696847af9afdf5e3623aa59af3
Joel Lappalainen <lappalj8@gmail.com>
2023-05-15 14:45:22 +0300

    Replace request with axios and update some dependencies

--- src/dockerRepo.js
+++ src/dockerRepo.js
@@ -1,13 +1,13 @@
 const drc = require('docker-registry-client')
 const debug = require('debug')('digitransit-deployer-repo')
-const rp = require('request-promise')
+const axios = require('axios')
 
 module.exports = {
   getImageDate: (repoAndRef) => {
     let rar = drc.parseRepoAndRef(repoAndRef)
     let url = `https://hub.docker.com/v2/repositories/${rar.remoteName}/tags/${rar.tag}`
-    return rp(url).then(res => {
-      const data = JSON.parse(res)
+    return axios.get(url).then(res => {
+      const data = res
       return Date.parse(data.last_updated)
     })
   },
