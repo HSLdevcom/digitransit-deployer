4829d911b3b465006e8402c2d471b5067f25db55
Joel Lappalainen <lappalj8@gmail.com>
2023-05-16 10:28:20 +0300

    Use native fetch instead of axios

--- src/dockerRepo.js
+++ src/dockerRepo.js
@@ -1,16 +1,21 @@
 const debug = require('debug')('digitransit-deployer-repo')
-const axios = require('axios')
 
 module.exports = {
   getImageDate: (repoAndRef) => {
     const [ repository, tag ] = repoAndRef.split(':')
     const url = `https://hub.docker.com/v2/repositories/${repository}/tags/${tag || 'latest'}`
-    return axios.get(url).then(res => {
-      if (res.status === 200) {
-        return Date.parse(res.data.last_updated)
+    return fetch(url).then(res => {
+      if (res.ok) {
+        return res.json()
       } else {
         debug(`failed to fetch data for ${repoAndRef} from docker hub`)
       }
     })
+      .then(data => {
+        return Date.parse(data.last_updated)
+      })
+      .catch(err => {
+        debug(err)
+      })
   }
 }
